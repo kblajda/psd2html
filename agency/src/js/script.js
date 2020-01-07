255275fc3dd08b20f5506e6e336945d806455838
kblajda <k.blajda1@gmail.com>
2020-01-07 22:10:13 +0100

    Done header section

--- agency/src/js/script.js
+++ agency/src/js/script.js
@@ -1,5 +1,12 @@
-const person = {
-    name: "Krystian",
-    age: 25
-};
-console.log(person.name);
+const scrollMenu = function () {
+    let menu = document.querySelector('nav');
+    if (window.scrollY > 90) {
+
+        console.log('scroll');
+        menu.classList.add('menu__scroll');
+    } else {
+        menu.classList.remove('menu__scroll');
+    }
+    console.log(scrollY)
+}
+window.addEventListener('scroll', scrollMenu);
